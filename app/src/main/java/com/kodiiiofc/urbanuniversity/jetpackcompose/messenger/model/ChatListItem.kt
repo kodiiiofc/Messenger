3efc96af95dbeb6cccc117bba3494cd591b165da
Roman Mokhnatkin <kodiiiofc@gmail.com>
2025-02-04 22:01:51 +0500

    удалены комментарии

--- app/src/main/java/com/kodiiiofc/urbanuniversity/jetpackcompose/messenger/model/ChatListItem.kt
+++ app/src/main/java/com/kodiiiofc/urbanuniversity/jetpackcompose/messenger/model/ChatListItem.kt
@@ -7,17 +7,4 @@ data class ChatListItem(
     val user: UserModel,
     val otherUser: UserModel,
     val lastMessage: MessageModel
-) {
-//    override fun equals(other: Any?): Boolean {
-//        return if (other is ChatListItem)
-//         (this.user == other.user) && (this.otherUser == other.otherUser)
-//        else false
-//    }
-//
-//    override fun hashCode(): Int {
-//        var result = user.hashCode()
-//        result = 31 * result + otherUser.hashCode()
-//        result = 31 * result + lastMessage.hashCode()
-//        return result
-//    }
-}
+)
