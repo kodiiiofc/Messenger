ab0574732da5a2287bf94dee1c2834be8da37665
Roman Mokhnatkin <kodiiiofc@gmail.com>
2025-02-04 22:14:28 +0500

    удалены комментарии и лишняя модель

--- app/src/main/java/com/kodiiiofc/urbanuniversity/jetpackcompose/messenger/model/TabItem.kt
+++ app/src/main/java/com/kodiiiofc/urbanuniversity/jetpackcompose/messenger/model/TabItem.kt
@@ -1,6 +1,5 @@
 package com.kodiiiofc.urbanuniversity.jetpackcompose.messenger.model
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class TabItem(
